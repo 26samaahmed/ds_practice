60b78b8fb2ed815d9b5c3bbbbd847de7ef4bc028
Sama Ahmed <26samahmed@csu.fullerton.edu>
2023-08-15 15:41:52 -0700

    practice linked list operations in progress

--- practice3.cc
+++ practice3.cc
@@ -44,6 +44,78 @@ void CheckBalance(std::string parantheses) {
   }
 }
 
+struct Node {
+  int data;
+  Node* next;
+};
+Node* head = nullptr;
+
+void InsertBegin(int value) {
+  Node* new_node = new Node(); // creating a pointer and allocating in the heap
+  new_node->data = value; // assign the value we're passing into the function argument when we call it
+  if (head == nullptr) { // if the list is empty
+    head = new_node; // the new_node becomes the head (first node in the list)
+    return;
+  } else{  // if the list is not empty
+    // we make the new node point at the head
+    new_node->next = head;
+    head = new_node; // now the new node becomes the head since it's the first node in the list
+  }
+}
+
+void InsertMiddle(int value, int position) {
+  Node* new_node = new Node();
+  new_node->data = value;
+  // i have to traverse through the list
+  Node* temp = head;
+  // check if the list is empty because we wouldn't be able to insert in the middle
+  if (head == nullptr || position == 1) {
+    new_node->next = head;
+    head = new_node;
+    return;
+  }
+
+  for (int i = 2; i < position; i++) {
+    temp = temp->next;
+  }
+  new_node->next = temp->next; // so if we have 1 -> 2 -> 4 and we want to insert 3 between 2 and 4, then we want 3 to point at whatever 2 is pointing at
+  temp->next = new_node;
+}
+
+void InsertEnd(int value) {
+  Node* new_node = new Node();
+  new_node->data = value;
+  new_node->next = nullptr;
+  Node* temp = head;
+  if (head == nullptr) { // if the list has no nodes yet
+    head = new_node;
+  }
+  // traverse through the whole list
+  while (temp->next != nullptr) {
+    temp = temp->next;
+  }
+  temp->next = new_node; // then we make the last node point to our new_node
+}
+
+
+void DisplayList() {
+  Node* temp = head;
+  while (temp != nullptr) {
+    std::cout << "node is " << temp->data << "\n";
+    temp = temp->next;
+  }
+}
+
+
+// inserting at begin (done)
+// inserting at middle (done)
+// inserting at end (done)
+// deleting in a linked list
+// reversing a linked list
+// merging 2 sorted lists
+// intersection of 2 linked lists
+// checking if a linked list is a palindrome
+
 
 int main() {
   CheckBalance("([]{()})"); // expected output: true (passed)
@@ -57,5 +129,12 @@ int main() {
   CheckBalance("{[({})]}"); // expected output: true (passed)
   CheckBalance("abcdef"); // expected output: true (passed)
   CheckBalance("a(b)c[d]e{f}"); // expected output: true (passed)
+  InsertBegin(5);
+  InsertBegin(4);
+  InsertBegin(3);
+  InsertMiddle(9, 1);
+  InsertEnd(100);
+  DisplayList();
+
   return 0;
 }
