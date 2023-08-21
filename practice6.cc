3f93ff680fc197b3f557a3befcdb62daa5a96616
Sama Ahmed <26samahmed@csu.fullerton.edu>
2023-08-20 22:03:04 -0700

    practicing queues

--- practice6.cc
+++ practice6.cc
@@ -31,6 +31,45 @@ void Reverse(std::string word) {
   std::cout << "reverse word is: " << word << "\n";
 }
 
+struct Node {
+  int data;
+  Node* next;
+};
+
+// 2 pointers, one for the start of the list and one for the end of the list
+Node* front;
+Node* back;
+
+// inserting in a queue
+void InsertQueue(int value) {
+  // check if the list is empty, if it is, then we set the back pointer to become the new node and then make front = back because there is only 1 node.
+  if (front == nullptr) {
+    back = new Node();
+    back->data = value;
+    back->next = nullptr;
+    front = back;
+  } else {
+    Node* new_node = new Node();
+    new_node->data = value;
+    new_node->next = nullptr;
+    // traverse through the whole list until i reach the last node which points at nullptr so i can make it point at the new node instead
+    Node* temp = front;
+    for (temp->next != nullptr) {
+      temp = temp->next;
+    }
+    // once the loop terminates, that means we reached the last node
+    // make the last node point at the new node
+    temp->next = new_node;
+    // now the new node is that back
+    back = new_node;
+  }
+  // if the list is is not empty, then pushing an element meaning adding it to the end as the new back where the original 
+  // last node would point at the new node and the new last node would point at nullptr
+
+}
+
+
+// deleting in a queue
 
 int main() {
   // test cases
